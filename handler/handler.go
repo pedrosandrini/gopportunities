4244615661dddf6b723955b626798c4abf9f41de
Pedro Sandrini <sandrini.pedro@outlook.com>
2024-01-19 10:49:05 -0300

    Feat: add separate files for handler functions

--- handler/handler.go
+++ handler/handler.go
@@ -6,32 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CREATE opening job",
-	})
-}
 
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE opening job",
-	})
-}
 
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "UPDATE opening job",
-	})
-}
 
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening jobs",
-	})
-}
 
-func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET all opening job",
-	})
-}
+
+
+
+
+

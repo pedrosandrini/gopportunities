35dcba0e65a983e93858c1f974212a123ea7e5da
Pedro Sandrini <sandrini.pedro@outlook.com>
2024-01-21 01:38:39 -0300

    Feat: add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -17,7 +17,7 @@ type CreateOpeningRequest struct {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
-	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Link != "" && r.Salary <= 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
 	if r.Role == "" {
@@ -39,4 +39,24 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+// Update Opening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validations is true
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
 }
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -1,13 +1,62 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pedrosandrini/gopportunities/schemas"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "UPDATE opening job",
-	})
+	request := UpdateOpeningRequest{}
+
+	// Faz o bind dos dados JSON da solicitação HTTP para a estrutura 'UpdateOpeningRequest' armazenada em request
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.ErrorF("validation erro: %v", err)
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.ErrorF("error updating opening; %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error on update opening")
+		return
+	}
+
+	sendSuccess(ctx, "update-opening", opening)
+
 }
